5a2c619ea65acc3a6da194ab84f6af0c08826403
meow <meow@>
2023-11-27 11:12:36 -0800

    removed type due to w3 suggestion

--- constants/favicon.ts
+++ constants/favicon.ts
@@ -1,7 +1,7 @@
 /** A minimal favicon in base64 to appease the browser. 
  * @example
  * ```tsx
- * <link rel="icon" type="image/png" href={favicon} />
+ * <link rel="icon" href={favicon} />
  * ```
 */
 export const favicon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAAASUVORK5CYII="
