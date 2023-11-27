ffb360728d9525aaee5c5d36c496e6da60eec064
meows <meows@users.noreply.github.com>
2023-11-27 11:09:06 -0800

    + [bots, favicon, headers] (#1)

---
+++ constants/headers.ts
@@ -0,0 +1,6 @@
+// —————————————————————————————————————————————————————————————————————————————
+// HTTP Content-Type Headers
+
+export const json = "application/json;charset=UTF-8"
+export const html = "text/html;charset=UTF-8"
+export const text = "text/plain;charset=UTF-8"
