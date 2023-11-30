88fa5e445dde881ea189f7d29d2de60dac87aa1f
meow <meow@>
2023-11-29 17:28:45 -0800

    ++ [gitignore, headers]

--- constants/headers.ts
+++ constants/headers.ts
@@ -4,3 +4,4 @@
 export const json = "application/json;charset=UTF-8"
 export const html = "text/html;charset=UTF-8"
 export const text = "text/plain;charset=UTF-8"
+export const svg = "image/svg+xml;charset=UTF-8"
